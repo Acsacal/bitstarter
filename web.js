32b6ec2e5351d2afce9bb2c98d9e28193548ead4
Ubuntu <ubuntu@ip-172-31-37-225.eu-west-1.compute.internal>
2013-07-09 20:28:35 +0000

    web.js mod

--- web.js
+++ web.js
@@ -2,8 +2,21 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var str = '';
+
+var str_gen = function () {
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  return(data.toString('utf-8'));
+});
+};
+
+str = str_gen();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
